21f80f06209d855a79b78171a74f6754e316f627
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-07 23:11:44 -0600

    add documentation

--- SwiftChallenge23/SwiftChallenge23/Places/ButtonView.swift
+++ SwiftChallenge23/SwiftChallenge23/Places/ButtonView.swift
@@ -5,9 +5,12 @@
 //  Created by Marco Ramirez on 7/04/23.
 //
 
+//Defined a simple ButtonView component that displays an image and text inside a button view. It can be customized by passing different icon and text values.
+
 import SwiftUI
 
 struct ButtonView: View {
+//declares two constant properties of type String that are used to store the icon and text to be displayed in the button.
     let icon: String
     let text: String
     
@@ -28,4 +31,3 @@ struct ButtonView_Previews: PreviewProvider {
         ButtonView(icon: "car", text: "Drive")
     }
 }
-
