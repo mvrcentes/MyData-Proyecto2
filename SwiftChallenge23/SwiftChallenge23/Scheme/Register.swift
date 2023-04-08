5abc7d4eae12fd11e0d814703143383662777f05
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-07 21:38:32 -0600

    add register scheme

---
+++ SwiftChallenge23/SwiftChallenge23/Scheme/Register.swift
@@ -0,0 +1,18 @@
+//
+//  Register.swift
+//  SwiftChallenge23
+//
+//  Created by Marco Ramirez on 7/04/23.
+//
+
+import SwiftUI
+
+struct Register: Identifiable {
+    var id: Int
+    var Doctorname: String
+    var entity: String
+    var date: Date
+    var medicine: [String]
+    var note: String
+}
+
---
+++ SwiftChallenge23/SwiftChallenge23/SwiftChallenge23App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChallenge23App.swift
+//  SwiftChallenge23
+//
+//  Created by Marco Ramirez on 7/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChallenge23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
